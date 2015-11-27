1690b1aeff54b08cfd3a634c3e25aa4e7b67e989
pixelTitan <pixelTitan@gmail.com>
2015-11-27 13:19:00 -0500

    Finished Problem 1
    Finished implementing the logic to solve problem #1.

--- Problem_1/main.cpp
+++ Problem_1/main.cpp
@@ -3,6 +3,16 @@
 using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
+
+    int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+        if (i % 3 != 0 && i % 5 != 0) {
+            continue;
+        }
+
+        sum += i;
+    }
+
+    cout << sum << endl;
     return 0;
 }
